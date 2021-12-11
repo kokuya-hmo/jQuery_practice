1696d3e83f781657e631c9c7e23474ce86fa8e5c
kokuya-hmo <88o.clock@gmail.com>
2021-12-12 02:59:31 +0900

    jquery_work00

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,5 +1,5 @@
 $(function(){
-  let pageCount = 1 ;
+  let pageCount = 1;
   //pageCountを1と設定
   let presearchWord = "";
   //presearchWordに空の変数を定義
@@ -17,34 +17,33 @@ $(function(){
       pageCount = pageCount + 1;
       //pageCountに1を足す
     }
-    const settings = {
+    const settings ={
       "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
       "method": "GET",
     }
-// Ajaxの実行
-//.doneが通信成功した時の処理、”response”が引数となっていて通信した結果を受け取っている
-    $.ajax(settings).done(function (response) {
+    // Ajaxの実行
+    //.doneが通信成功した時の処理、”response”が引数となっていて通信した結果を受け取っている
+    $.ajax(settings).done(function (response){
       const result = response['@graph'];
       //resultに[@graph]データを格納
       displayResult(result)
       //deisplayResultを実行
-    }).fail(function (err) {
-  //.failが通信に失敗した時の処理、”err”にサーバーから送られてきたエラー内容を受け取っている
+    }).fail(function (err){
+    //.failが通信に失敗した時の処理、”err”にサーバーから送られてきたエラー内容を受け取っている
       displayError(err)
     });
   });
-  function displayResult(x){
+  function displayResult(result){
     //下記の処理をdisplayResulC:\Users\hmomo\prs-work\jQuery\実装\jquery_practice\q8\work\index.htmltという変数に格納
     $(".message").remove();
     //messageのクラス名の要素を削除
-    if (x[0].items !== undefined){
+    if (result[0].items !== undefined){
     //x[0].itemsが定義されていないときに処理に入る
-      $.each(x[0].items,function(index,value){
-        //引数xに対しての繰り返し処理
+      $.each(result[0].items,function(index,value){
+      //引数xに対しての繰り返し処理
         const displayText = '<li class="lists-item"><div class="list-inner"><p>タイトル:' + ((value.title ? value.title :"作者ふめい")+ "</p>" +
-                              "<p>作者:") + ((value["dc:creator"] ? value["dc:creator"]:"タイトル不明")+ "</p>" + "<p>出版社:") + ((value["dc:publisher"] ? value["dc:publisher"] :"出版社不明") + "</p>" +
-                              "<a href=" + value.link["@id"] + ">書籍情報</a></div></li>");
-        //listクラスに対して初めにdisplayTextに格納した要素を追加
+                            "<p>作者:") + ((value["dc:creator"] ? value["dc:creator"]:"タイトル不明")+ "</p>" + "<p>出版社:") + ((value["dc:publisher"] ? value["dc:publisher"] :"出版社不明") + "</p>" +
+                            "<a href=" + value.link["@id"] + ">書籍情報</a></div></li>");
         $(".lists").prepend(displayText);
         //listsクラス名のと要素の最初にdisplayTextに格納した要素を追加
       });
@@ -53,17 +52,19 @@ $(function(){
       //それ以外の場合message追加
     }
   };
-  function  displayError() {
-    //関数名displayerrorに以下の処理を格納
-    $(".lists").empty();
-    //lists要素を空に
-    $(".message").remove();
-    //message要素を空に
-    $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>');
-    //messageを追加
+  function  displayError(err){
+  //関数名displayerrorに以下の処理を格納
+      if(err.status === 0){
+        $(".lists").empty();
+        //lists要素を空に
+        $(".message").remove();
+        //message要素を空に
+        $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>');
+        //messageを追加
+      }
   };
   $(".reset-btn").on("click",function(){
-    //reset-btnが押されたときに実行
+  //reset-btnが押されたときに実行
     $(".lists").empty();
     //lists要素を空に
     $(".message").remove();
@@ -74,5 +75,5 @@ $(function(){
     //presearchWordを空に
     pageCount = 1;
     //pageCountを1とする
-  })
+  });
 });
